3c25cc3a6a993502794b2488dd8b91c634cd7105
wickedmc4ever <153628135+wickedmc4ever@users.noreply.github.com>
2023-12-13 21:20:05 +0800

    Add files via upload

---
+++ shuixiaoxuan/server/crud.py
@@ -0,0 +1,8 @@
+# crud.py
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+# 更多session API
+# 参考：https://docs.sqlalchemy.org/en/20/orm/session_api.html#sqlalchemy.orm.Session
