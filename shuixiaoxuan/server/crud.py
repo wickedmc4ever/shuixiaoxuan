08c988f912dd85fbbb5c70268e22e13e71c7c56d
sherylcoding <153628135+wickedmc4ever@users.noreply.github.com>
2023-12-18 21:18:21 +0800

    Delete shuixiaoxuan /directory and renew project .

--- shuixiaoxuan/server/crud.py
+++
@@ -1,8 +0,0 @@
-# crud.py
-from sqlalchemy.orm import Session
-
-from . import models, schemas
-
-
-# 更多session API
-# 参考：https://docs.sqlalchemy.org/en/20/orm/session_api.html#sqlalchemy.orm.Session
--- shuixiaoxuan/server/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-# Define your schemas here
-
-class MySchema(BaseModel):
-    # Define the fields for your schema here
-    pass
-
-# Add more schemas as needed
